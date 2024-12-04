d92e4e2a5beb4db77775ceef84060f0a9910075a
Sirada U <nan.uthaiwat@gmail.com>
2024-12-05 00:33:40 +0700

    feat: add team members

---
+++ src/app/components/Members.tsx
@@ -0,0 +1,24 @@
+export default function Members() {
+    const list =[
+        {"name" : "ณภัทษร สุกใส", "nickname" : "กึ๊น", "email": "keinkunluk@gmail.com", "ig":"kein._.stagram"},
+        {"name" : "ธัทเดช กัลยพฤกษ์", "nickname" : "เอส", "email": "kanlayphrukthaddej@gmail.com", "ig":"ยังไม่ได้ใส่"},
+        {"name" : "พิชชาภา ลักษณ์เลิศกุล", "nickname" : "ชูก้า", "email": "pidchapa.lucklertkul@docchula.com", "ig":"ยังไม่ได้ใส่"},
+        {"name" : "ศิรดา อุทัยวัฒน์", "nickname" : "แนน", "email": "sirada.uth@docchula.com", "ig":"sira.uth"},
+        {"name" : "จันทร์วลัย เลี่ยนเครือ", "nickname" : "เกน", "email": "jennissa0811@docchula.com", "ig":"ยังไม่ได้ใส่"},
+        {"name" : "วรวีร์ พัวพรพงษ์", "nickname" : "วินชี่", "email": "worawee.pua@docchula.com", "ig":"ยังไม่ได้ใส่"},
+        {"name" : "กัณณพงศ์ กาญจนกุลดำรง", "nickname" : "อาอัส", "email": "1234kannapongkarn@docchula.com", "ig":"ยังไม่ได้ใส่"},
+        {"name" : "ศศิษา ปานพรหมมินทร์", "nickname" : "บลิ้งค์", "email":"sasisa.blink@dochula.com", "ig":"sasisap"}
+    ]
+    return (
+        <>
+            {list.map((item, index)=>(
+                <div className="mt-5" key={item.name}>
+                    <p>profile picture</p>
+                    <p>{item.name}</p>
+                    <p>E-Mail : {item.email}</p>
+                    <p>IG : {item.ig}</p>
+                </div>
+        ))}
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "./components/Navbar";
+import Members from "./components/Members";
+
 // import smcu from "/public/smcu.png"
 
 //*****แก้*********
@@ -83,144 +85,7 @@ export default function Home() {
           <div className="text-2xl font-bold pt-5"> Team members</div>
         </h2>
 
-        <h4>ชั้นปีที่ 1</h4>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>ณภัทษร สุกใส (กึ๊น)</h4>
-                <div>
-                  Email : keinkunluk@gmail.com
-                  <div>
-                    Ig :
-                    <a href="https://www.instagram.com/kein._.stagram?igsh=MTBveWN0d2trMTNoeQ==">
-                      kein._.stagram
-                    </a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>ธัทเดช กัลยพฤกษ์(เอส)</h4>
-                <div>
-                  Email : kanlayphrukthaddej@gmail.com
-                  <div>
-                    Ig :<a href="8">ยังไม่ได้ใส่จ้า</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>พิชชาภา ลักษณ์เลิศกุล(ชูก้า)</h4>
-                <div>
-                  Email : pidchapa.lucklertkul@docchula.com
-                  <div>
-                    Ig :<a href="8">ยังไม่ได้ใส่จ้า</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>ศิรดา อุทัยวัฒน์ (แนน)</h4>
-                <div>
-                  Email : sirada.uth@docchula.com
-                  <div>
-                    Ig :
-                    <a href="https://www.instagram.com/sira.uth?igsh=MXhmYW5lOGFvMGZ6OQ%3D%3D&utm_source=qr">
-                      sira.uth
-                    </a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>จันทร์วลัย เลี่ยนเครือ (เกน)</h4>
-                <div>
-                  Email : jennissa0811@docchula.com
-                  <div>
-                    Ig :<a href="8">ยังไม่ได้ใส่จ้า</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>วรวีร์ พัวพรพงษ์ (วินชี่)</h4>
-                <div>
-                  Email : worawee.pua@docchula.com
-                  <div>
-                    Ig :<a href="8">ยังไม่ได้ใส่จ้า</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>กัณณพงศ์ กาญจนกุลดำรง (อาอัส)</h4>
-                <div>
-                  Email : kannapongkarn@docchula.com
-                  <div>
-                    Ig :<a href="https://www.instagram.com/r_you.s/">r_you.s</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="item">
-          <div className="content">
-            <ul>
-              <li>
-                {/* <Image src="/smcu.png" width={50} height={50}/> */}
-                <h4>ศศิษา ปานพรหมมินทร์ (บลิ้งค์)</h4>
-                <div>
-                  Email : sasisa.blink@dochula.com
-                  <div>
-                    Ig :
-                    <Link href="https://www.instagram.com/sasisap?igsh=aGZuM2Nudnd0cXJj">
-                      sasisap
-                    </Link>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <Members />
       </div>
     </main>
   );
