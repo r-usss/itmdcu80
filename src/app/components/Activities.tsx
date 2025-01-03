dd8c85091fdb6f2f93bee521bfc7c9b7b25985ed
Sirada U <nan.uthaiwat@gmail.com>
2025-01-03 19:13:34 +0700

    feat: add activities card

--- src/app/components/Activities.tsx
+++ src/app/components/Activities.tsx
@@ -1,77 +1,61 @@
 import { activitiesList } from "../constants/actlist";
+import ActivitiesCard from "./ActivitiesCard";
 export default function Activities() {
-//import Image from "next/image";
-//import syringeLogo from "/image/syringe-logo.webp";
+  //import Image from "next/image";
+  //import syringeLogo from "/image/syringe-logo.webp";
   return (
     <>
-    <div className="text-white dark:text-dark dark:bg-pink-400 " >
+      <div className="text-white dark:text-dark dark:bg-pink-400 ">
+        <div className="text-2xl font-bold py-10">Recent Activities</div>
 
-      <div className="text-2xl font-bold py-10">Recent Activities</div>
+        {/* test */}
+        {/* {activitiesList.map((actItem, actIndex) => (
+          <ActivitiesCard
+            status={actItem.status}
+            image={actItem.image}
+            act={actItem.act}
+            detail={actItem.detail}
+            topic="finished"
+          />
+        ))} */}
 
-      {/* เสร็จแล้ว */}
-      <strong > ทำแล้ว </strong>
-      
-      {activitiesList.map((actItem, actIndex) => (
-        <div className=" flex mt-5 mx-2 inline-block">
-          {actItem.status === "finished" && (
-            <div className="w-3/6 bg-white dark:text-white dark:bg-gray-500 text-black rounded-xl text-left pl-3 ">
-              <img 
-            className=" mb-2 mt-2 mr-10 float-left rounded-xl"
-            src={actItem.image}
-            alt={"pic"}
-            width={96} 
-            height={96} 
-            />
-              <p className="text-xl pt-3">{actItem.act}</p>
-              <p>{actItem.detail}</p>
-        </div>
-          )}
+        {/* เสร็จแล้ว */}
+        <strong> ทำแล้ว </strong>
+
+        {activitiesList.map((actItem, actIndex) => (
+          <ActivitiesCard
+            status={actItem.status}
+            image={actItem.image}
+            act={actItem.act}
+            detail={actItem.detail}
+            topic="finished"
+          />
+        ))}
+
+        {/* ทำอยู่ */}
+
+        <strong className="text-left"> ทำอยู่ </strong>
+
+        {activitiesList.map((actItem, actIndex) => (
+          <ActivitiesCard
+            status={actItem.status}
+            image={actItem.image}
+            act={actItem.act}
+            detail={actItem.detail}
+            topic="ongoing"
+          />
+        ))}
+        <strong> ทำต่อ </strong>
+        {activitiesList.map((actItem, actIndex) => (
+          <ActivitiesCard
+            status={actItem.status}
+            image={actItem.image}
+            act={actItem.act}
+            detail={actItem.detail}
+            topic="continue"
+          />
+        ))}
       </div>
-        
-      ))}
-
-      {/* เสร็จแล้ว */}
-      
-      <strong className="text-left"> ทำอยู๋ </strong>
-      
-      {activitiesList.map((actItem, actIndex) => (
-        <div className=" flex my-5 mx-2 inline-block">
-          {actItem.status === "ongoing" && (
-            <div className="w-3/6 bg-white dark:text-white dark:bg-gray-500 text-black rounded-xl text-left pl-3 ">
-              <img 
-            className=" mb-2 mt-2 mr-10 float-left rounded-xl"
-            src={actItem.image}
-            alt={"pic"}
-            width={96} 
-            height={96} 
-            />
-              <p className="text-xl pt-3">{actItem.act}</p>
-              <p>{actItem.detail}</p>
-            </div>
-          )}
-        </div>
-        //flex flex-col
-      ))}
-      <strong > ทำต่อ </strong>
-      {activitiesList.map((actItem, actIndex) => (
-        <div className="flex mt-5 mx-2 inline-block">
-          {actItem.status === "continue" && (
-            <div className="w-3/6 bg-white dark:text-white dark:bg-gray-500 text-black rounded-xl text-left pl-3 ">
-              <img 
-            className=" mb-2 mt-2 mr-10 float-left rounded-xl"
-            src={actItem.image}
-            alt={"pic"}
-            width={96} 
-            height={96} 
-            />
-              <p className="text-xl pt-3">{actItem.act}</p>
-              <p>{actItem.detail}</p>
-            </div>
-          )}
-        </div>
-        
-      ))}
-    </div>
     </>
   );
 }
