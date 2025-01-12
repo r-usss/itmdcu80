d8582e27b1f8272617e4278bd63b2ec4b0477888
BLKdrone <sasisa.blink@gmail.com>
2025-01-12 16:03:18 +0700

    pnpm updates

--- src/app/components/Activities.tsx
+++ src/app/components/Activities.tsx
@@ -54,9 +54,9 @@ export default function Activities() {
       ))}
       <strong > ทำต่อ </strong>
       {activitiesList.map((actItem, actIndex) => (
-        <div className="flex mt-5 mx-2 inline-block">
+        <div className="flex mt-4 mx-2 inline-block">
           {actItem.status === "continue" && (
-            <div className="w-3/6 bg-white dark:text-white dark:bg-gray-500 text-black rounded-xl text-left pl-3 ">
+            <div className="w-3/6 bg-white dark:text-white dark:bg-gray-500 text-black rounded-xl text-left pl-3  ">
               <img 
             className=" mb-2 mt-2 mr-10 float-left rounded-xl"
             src={actItem.image}
@@ -69,8 +69,8 @@ export default function Activities() {
             </div>
           )}
         </div>
-        
       ))}
+    
     </div>
     </>
   );
