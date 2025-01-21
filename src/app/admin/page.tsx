47d680ccc0363e70e1864bf99b6bda4e11074278
Sirada U <nan.uthaiwat@gmail.com>
2025-01-21 21:14:39 +0700

    feat: read data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function page() {
+"use client";
+import React, { useState, useEffect } from "react";
+export default function Page() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://www.melivecode.com/api/users")
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+        setUsers(result)
+      });
+    // console.log("hello");
+  }, []);
   return (
-    <div>For Admin</div>
-  )
+    <>
+      <div>Admin</div>
+      {JSON.stringify(users)}
+    </>
+  );
 }
-
-export default page
